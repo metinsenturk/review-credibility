3ecba9b0dd344778e8c920c0a332a4e048b19a70
Nandini Khatod <nandinikhatod@yahoo.in>
2019-10-12 19:35:43 -0400

    yelp  ddl updated

--- sql/yelp-ddl.sql
+++ sql/yelp-ddl.sql
@@ -22,34 +22,40 @@ CROSS APPLY OPENJSON(BulkColumn)
  outer apply openjson(cat) with ( cat_ nvarchar(8) '$')
 ;
 -------------------------------------------------BUSINESS------------------------------------------------------------------------------
-DROP TABLE IF EXISTS Business
-CREATE TABLE Business (
- business_id nvarchar(100) NULL, 
- name nvarchar(100) NULL, 
- address nvarchar(100) NULL,
- city nvarchar(100) NULL,
- state nvarchar(20) NULL,
- postal_code int NULL,
- latitude float NULL,
- longitude float NULL,
- stars float NULL,
- review_count int NULL,
- is_open int NULL,
- RestaurantsTakeOut nvarchar(20) NULL,
- BusinessParking_garage nvarchar(20) NULL,
- BusinessParking_street nvarchar(20) NULL,
- BusinessParking_validated nvarchar(20) NULL,
- BusinessParking_lot nvarchar(20) NULL,
- BusinessParking_valet nvarchar(20) NULL,
- categories nvarchar(max) NULL,
- hours nvarchar(max) NULL,
- ) 
+DROP TABLE IF EXISTS Businesses
+CREATE TABLE Businesses (
+	business_id nvarchar(100) NULL, 
+	name nvarchar(100) NULL, 
+	address nvarchar(100) NULL,
+	city nvarchar(100) NULL,
+	state nvarchar(20) NULL,
+	postal_code int NULL,
+	latitude float NULL,
+	longitude float NULL,
+	stars float NULL,
+	review_count int NULL,
+	is_open int NULL,
+	RestaurantsTakeOut nvarchar(20) NULL,
+	BusinessParking_garage nvarchar(20) NULL,
+	BusinessParking_street nvarchar(20) NULL,
+	BusinessParking_validated nvarchar(20) NULL,
+	BusinessParking_lot nvarchar(20) NULL,
+	BusinessParking_valet nvarchar(20) NULL,
+	categories nvarchar(max) NULL,
+	monday nvarchar(200) NULL,
+	tuesday nvarchar(200) NULL,
+	wednesday nvarchar(200) NULL,
+	thursday nvarchar(200)  NULL,
+	friday nvarchar(200) NULL,
+	saturday nvarchar(200) NULL,
+	sunday nvarchar(200) NULL,
+) 
 
-INSERT INTO Business
-SELECT * FROM
+INSERT INTO Businesses
+SELECT business.* FROM
 
 OPENROWSET(
-	BULK 'business.json', 
+	BULK 'business_x.json', 
 	DATA_SOURCE = 'YelpJsonFiles',
 	SINGLE_CLOB
 ) AS json
@@ -72,7 +78,13 @@ CROSS APPLY OPENJSON(BulkColumn)
 		BusinessParking_lot nvarchar(20) '$.attributes.BusinessParking.lot',
 		BusinessParking_valet nvarchar(20) '$.attributes.BusinessParking.valet',
 		categories nvarchar(max) as json,
-		hours nvarchar(max) as json,
+		monday nvarchar(200) '$.hours.Monday',
+		tuesday nvarchar(200) '$.hours.Tuesday',
+		wednesday nvarchar(200) '$.hours.Wednesday',
+		thursday nvarchar(200) '$.hours.Thursday',
+		friday nvarchar(200) '$.hours.Friday',
+		saturday nvarchar(200) '$.hours.Saturday',
+		sunday nvarchar(200) '$.hours.Sunday'
  ) as business
 	  
  --outer apply openjson(categories) with ( cat_ nvarchar(100) '$')
@@ -82,8 +94,8 @@ CROSS APPLY OPENJSON(BulkColumn)
 
 ----------------------------------------------------------REVIEW ------------------------------------------------------
 
-DROP TABLE IF EXISTS Review
-CREATE TABLE Review (
+DROP TABLE IF EXISTS Reviews
+CREATE TABLE Reviews (
 		review_id nvarchar(100) ,
 		user_id nvarchar(100),
 		business_id nvarchar(100),
@@ -94,92 +106,96 @@ CREATE TABLE Review (
 		funny int,
 		cool int
 		)
-insert into Review
-SELECT * FROM
+insert into Reviews
+SELECT review.* FROM
 
 OPENROWSET(
-	BULK 'review.json', 
+	BULK 'review_x.json', 
 	DATA_SOURCE = 'YelpJsonFiles',
 	SINGLE_CLOB
 ) AS json
 CROSS APPLY OPENJSON(BulkColumn)
- WITH( review_id nvarchar(100) ,
-		user_id nvarchar(100),
-		business_id nvarchar(100),
-		stars float,
-		date date,
-		text nvarchar(max),
-		useful int,
-		funny int,
-		cool int ) as review
+WITH( 
+	review_id nvarchar(100),
+	user_id nvarchar(100),
+	business_id nvarchar(100),
+	stars float,
+	date date,
+	text nvarchar(max),
+	useful int,
+	funny int,
+	cool int
+) as review
 ;
 
---------------------------------------------------CHECKING----------------------------------------------------------------------------
-DROP TABLE IF EXISTS Checking
-CREATE TABLE Checking (
+--------------------------------------------------CHECKIN----------------------------------------------------------------------------
+DROP TABLE IF EXISTS Checkins
+
+CREATE TABLE Checkins (
 	business_id nvarchar(100),
-	date nvarchar(100)  -- list of date in csv format
-	)
-insert into Checking
-SELECT * FROM
+	date nvarchar(max)  -- list of date in csv format
+)
+
+insert into Checkins
+SELECT checkin.* FROM
 
 OPENROWSET(
-	BULK 'checking.json', 
+	BULK 'checkin_x.json', 
 	DATA_SOURCE = 'YelpJsonFiles',
 	SINGLE_CLOB
 ) AS json
 CROSS APPLY OPENJSON(BulkColumn)
- WITH(	business_id nvarchar(100),
-		date nvarchar(100)
-
-		) as checking
+ WITH(
+	 business_id nvarchar(100),
+	 date nvarchar(max)
+) as checkin
 ;
 ---------------------------------------------------------TIP------------------------------------------------------------
-DROP TABLE IF EXISTS Tip
-CREATE TABLE Tip (
-	text nvarchar(100),
-	date date,  -- list of date in csv format
-	compliment_count int,
+DROP TABLE IF EXISTS Tips
+CREATE TABLE Tips (
 	business_id nvarchar(100),
-	user_id nvarchar(100)
+	user_id nvarchar(100),
+	date date,  -- list of date in csv format
+	text nvarchar(max),
+	compliment_count int,
 	)
-insert into Tip
+insert into Tips
 SELECT tip.* FROM
 
 OPENROWSET(
-	BULK 'tip.json', 
+	BULK 'tip_x.json', 
 	DATA_SOURCE = 'YelpJsonFiles',
 	SINGLE_CLOB
 ) AS json
 CROSS APPLY OPENJSON(BulkColumn)
- WITH(	text nvarchar(100),
-		date date,  -- list of date in csv format
-		compliment_count int,
-		business_id nvarchar(100),
-		user_id nvarchar(100) 
-		) as tip
+ WITH(
+	business_id nvarchar(100),
+	user_id nvarchar(100),
+	date date,  -- list of date in csv format
+	text nvarchar(max),
+	compliment_count int
+) as tip
 ----------------------------------------------------PHOTO----------------------------------------------------------------------
 
-DROP TABLE IF EXISTS Photo
-CREATE TABLE Photo (
+DROP TABLE IF EXISTS Photos
+CREATE TABLE Photos (
 		photo_id nvarchar(100),
 		business_id nvarchar(100),
-		caption nvarchar(100),
+		caption nvarchar(200),
 		label nvarchar(100)
 		)
 
-insert into Photo
-SELECT * FROM
+insert into Photos
+SELECT photo.* FROM
 
 OPENROWSET(
-	BULK 'photo.json', 
+	BULK 'photo_x.json', 
 	DATA_SOURCE = 'YelpJsonFiles',
 	SINGLE_CLOB
 ) AS json
 CROSS APPLY OPENJSON(BulkColumn)
  WITH(	photo_id nvarchar(100),
 		business_id nvarchar(100),
-		caption nvarchar(100),
+		caption nvarchar(200),
 		label nvarchar(100)
-
-		) as photo
+) as photo
